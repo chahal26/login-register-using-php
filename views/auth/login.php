dc9dae74883a8143de1613e5edfdbe727bfbb946
Sahil Chahal <dev.sahilchahal@gmail.com>
2022-12-27 12:13:07 +0530

    Added Login Form

--- views/auth/login.php
+++ views/auth/login.php
@@ -1,1 +1,16 @@
-<h2>Login Page</h2><hr/>
+<h2 class="mt-4">Login</h2><hr/>
+
+<div class="form">
+    <form action="" method="post">
+        <div class="mb-3 mt-3">
+            <label for="email" class="form-label">Email:</label>
+            <input type="email" class="form-control" id="email" placeholder="Enter email" name="email">
+        </div>
+        <div class="mb-3">
+            <label for="password" class="form-label">Password:</label>
+            <input type="password" class="form-control" id="password" placeholder="Enter password" name="password">
+        </div>
+
+        <button type="submit" class="btn btn-success">Submit</button>
+    </form>
+</div>
